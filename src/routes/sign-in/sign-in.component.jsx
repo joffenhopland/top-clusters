23d4255325ddcee32a4cc882ea301c8b5103f525
Jon-Fredrik Hopland <jho107@post.uit.no>
2022-12-23 14:30:45 +0100

    create google users firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,20 +1,20 @@
 import {
-    signInWithGooglePopup,
-    createUserProfileDocument,
-  } from '../../utils/firebase.utils';
-  
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase.utils';
+
 const SignIn = () => {
-    const logGoogleUser = async () => {
-      const response = await signInWithGooglePopup();
-      createUserProfileDocument(response);
-    };
-  
-    return (
-      <div>
-        <h1>Sign In Page</h1>
-        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      </div>
-    );
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
-  
-  export default SignIn;
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </div>
+  );
+};
+
+export default SignIn;
