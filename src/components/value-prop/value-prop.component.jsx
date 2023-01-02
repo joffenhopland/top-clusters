b384938f598cc4dfaa03c8ee8a7f0b55c7773875
Jon-Fredrik Hopland <jho107@post.uit.no>
2023-01-02 14:48:55 +0100

    value prop section

--- src/components/value-prop/value-prop.component.jsx
+++ src/components/value-prop/value-prop.component.jsx
@@ -1,9 +1,61 @@
-import {ValuePropContainer, ValuePropContent} from "./value-prop.styles"
+import {ValuePropContainer, ValuePropBackground, ValuePropContent, ValuePropTitle, ValuePropBenefitsContent, ValuePropBenefit1, ValuePropBenefit2, ValuePropBenefit3, ValuePropBenefit4, ValuePropBenefit5, ValuePropBenefit6, ValuePropBenefitTitle, ValuePropBenefitText, ValuePropIconContainer} from "./value-prop.styles"
+import { ReactComponent as TrendingUpIcon } from '../../assets/icons/trending_up_FILL0_wght400_GRAD0_opsz48.svg';
+import { ReactComponent as HubIcon } from '../../assets/icons/hub_FILL0_wght400_GRAD0_opsz48.svg';
+import { ReactComponent as OrganicTrafficIcon } from '../../assets/icons/organic_traffic_icon.svg';
+import { ReactComponent as AuthorityIcon } from '../../assets/icons/authority_icon.svg';
+import { ReactComponent as ContentIcon } from '../../assets/icons/content_icon.svg';
+import { ReactComponent as SearchIcon } from '../../assets/icons/search_icon.svg';
+
 
 const ValueProp = () => {
     return(
         <ValuePropContainer>
-            value pro
+            <ValuePropBackground>
+            </ValuePropBackground>
+            <ValuePropContent>
+                <ValuePropTitle>
+                    Our keyword clustering software is built to make your life easier
+                </ValuePropTitle>
+                <ValuePropBenefitsContent>
+                    <ValuePropBenefit1>
+                    <ValuePropIconContainer><TrendingUpIcon/></ValuePropIconContainer>
+                    <ValuePropBenefitTitle>Cluster thousands of keywords</ValuePropBenefitTitle>
+                    <ValuePropBenefitText>Automate the keyword research process and save hours of time by
+              allowing Top Clusters to group your keywords into topical clusters
+              based on user search intent.</ValuePropBenefitText>
+                    </ValuePropBenefit1>
+                    <ValuePropBenefit2>
+                    <ValuePropIconContainer><HubIcon/></ValuePropIconContainer>
+                    <ValuePropBenefitTitle>Improve your ranking in the SERPs</ValuePropBenefitTitle>
+                    <ValuePropBenefitText>We use real-time Google SERP data to quickly identify topically
+              related keywords by identifying URLs that are ranking for similar
+              keywords.</ValuePropBenefitText>
+                    </ValuePropBenefit2>
+                    <ValuePropBenefit3>
+                    <ValuePropIconContainer><OrganicTrafficIcon/></ValuePropIconContainer>
+                    <ValuePropBenefitTitle>Increase your organic traffic</ValuePropBenefitTitle>
+                    <ValuePropBenefitText>Our keyword clustering tool helps you optimize your content marketing for search intention, which can result in more organic traffic.</ValuePropBenefitText>
+                    </ValuePropBenefit3>
+                    <ValuePropBenefit4>
+                    <ValuePropIconContainer><AuthorityIcon/></ValuePropIconContainer>
+                    <ValuePropBenefitTitle>Build authority in your industry niche</ValuePropBenefitTitle>
+                    <ValuePropBenefitText>Accurate keyword grouping helps your website and content look more authoritative to search engine algorithms and visitors.</ValuePropBenefitText>
+                    
+                    </ValuePropBenefit4>
+                    <ValuePropBenefit5>
+                    <ValuePropIconContainer><ContentIcon/></ValuePropIconContainer>
+                    <ValuePropBenefitTitle>Enhance the context and relevance of your content</ValuePropBenefitTitle>
+                    <ValuePropBenefitText>Identify topically related keywords that you can target on a single page to offer more context to the readers and improve the relevance of your content.</ValuePropBenefitText>
+                    
+                    </ValuePropBenefit5>
+                    <ValuePropBenefit6>
+                    <ValuePropIconContainer><SearchIcon/></ValuePropIconContainer>
+                    <ValuePropBenefitTitle>Improve rankings for short and long-tailed keywords</ValuePropBenefitTitle>
+                    <ValuePropBenefitText>Using topic cluster increases your chances of ranking for long and short-tailed keywords that can be difficult to rank for alone.</ValuePropBenefitText>
+                    
+                    </ValuePropBenefit6>
+                </ValuePropBenefitsContent>
+            </ValuePropContent>
 
         </ValuePropContainer>
     )
--- src/components/value-prop/value-prop.styles.jsx
+++ src/components/value-prop/value-prop.styles.jsx
@@ -1,9 +1,104 @@
 import styled from 'styled-components';
 
 export const ValuePropContainer = styled.div`
+display: flex;
+justify-content: center;
+// padding: 56px 0 56px 0;
+position: relative;
+overflow: hidden;
+height: 100vh;
 `;
 
-export const ValuePropContent = styled.section`
+export const ValuePropBackground = styled.div`
+position: relative;
+top: 180px;
+left: 120px;
+width: 1524px;
+height: 1500px;
+background: radial-gradient(circle, rgba(215,152,225,1) 8%, rgba(155,255,165,1) 28%, rgba(174,211,255,1) 62%, rgba(201,212,239,1) 80%, rgba(202,207,250,1) 89%);
+// background: radial-gradient(483.9% 2719.65% at -49.5% -250%, #D798E1 17.55%, #9BFFA5 27.56%, #AED3FF 49.89%, #C9D4EF 56.53%, #CACFFA 65.69%)
+filter: blur(400px);
+// background-position: left;
+// background-repeat: no-repeat;
+// background-size: cover;
+`;
+
+export const ValuePropContent = styled.div`
+height: 100%;
+// width: 100%;
+position: absolute;
+top: 0;
+right: 0;
 display: flex;
 flex-direction: column;
+justify-content: center;
+align-items: center;
+text-align: center;
+padding: 48px 0 0 0;
+
+`;
+
+export const ValuePropTitle = styled.h2`
+width: 60%;
+font-size: ${(props) => props.theme.textStyles.h2.fontSize};
+font-weight: ${(props) => props.theme.textStyles.h2.fontWeight};
+color: ${(props) => props.theme.colors.primary};
+padding: 12px 0 38px 0;
+`;
+
+export const ValuePropBenefitsContent = styled.div`
+width: 80%;
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+grid-template-rows: repeat(2, 1fr);
+grid-column-gap: 56px;
+grid-row-gap: 36px;
+`;
+
+export const ValuePropBenefit1 = styled.div`
+grid-area: 1 / 1 / 2 / 2;
+text-align: start;
+`;
+export const ValuePropBenefit2 = styled.div`
+grid-area: 1 / 2 / 2 / 3;
+text-align: start;
+
+`;
+export const ValuePropBenefit3 = styled.div`
+grid-area: 1 / 3 / 2 / 4;
+text-align: start;
+
+`;
+export const ValuePropBenefit4 = styled.div`
+grid-area: 2 / 1 / 3 / 2;
+text-align: start;
+
+`;
+export const ValuePropBenefit5 = styled.div`
+grid-area: 2 / 2 / 3 / 3;
+text-align: start;
+
+`;
+export const ValuePropBenefit6 = styled.div`
+grid-area: 2 / 3 / 3 / 4;
+text-align: start;
+
+`;
+
+export const ValuePropBenefitTitle = styled.h4`
+font-size: ${(props) => props.theme.textStyles.h4.fontSize};
+font-weight: ${(props) => props.theme.textStyles.h4.fontWeight};
+line-height: ${(props) => props.theme.textStyles.h4.lineHeight};
+color: ${(props) => props.theme.colors.primary};
+margin: 12px 0;
+`;
+
+export const ValuePropBenefitText = styled.p`
+font-size: ${(props) => props.theme.textStyles.paragraphLarge.fontSize};
+font-weight: ${(props) => props.theme.textStyles.paragraphLarge.fontWeight};
+line-height: ${(props) => props.theme.textStyles.paragraphLarge.lineHeight};
+color: ${(props) => props.theme.colors.primary};
+`;
+
+export const ValuePropIconContainer = styled.div`
 `;
